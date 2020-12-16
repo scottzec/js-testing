e4761c68810a9000891d7ddd998c477c51d5c12f
Leah Scott-Zechlin <scottzec@gmail.com>
2020-12-15 18:24:02 -0800

    changed to for...of, no forEach

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,9 +1,19 @@
+const { TestScheduler } = require("jest");
+
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
 const isPangram = function(text) {
-  if (text.includes('x')) {
-    return true;
-  } else {
-    return false;
+
+  for (const alphaLetter of ALPHABET) {
+    if (!text.includes(alphaLetter)) {
+      return false;
+    }
   }
+
+  return true
+  
 };
+
+console.log(isPangram('the_quick brown fox jumps over the lazy dog'))
 
 module.exports = isPangram;
